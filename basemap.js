52dab541c7b92e58ed2cc277b0c882d10e8dfef4
Julian Zwerenz <julian.zw@googlemail.com>
2018-04-18 16:45:03 +0200

    Karte zu .js hinzugefuegt

--- basemap.js
+++ basemap.js
@@ -1,1 +1,6 @@
-alert("hallo welt");
+//Definition der Varibalen
+let myMap = L.map("mapdiv"); 
+let myLayer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+
+myMap.addLayer(myLayer); //Zusammenfügen des Layers zur Karte
+myMap.setView([47.267,11.383], 11); //Koordinaten setzen
