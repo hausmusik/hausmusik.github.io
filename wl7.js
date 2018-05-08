13fccb23e10ccd3bbec9e7b4206fcacbba4db2f1
Julian Zwerenz <julian.zw@googlemail.com>
2018-05-08 12:17:16 +0200

    wl7 alles auf einer Karte funktioniert nicht

--- wl7.js
+++ wl7.js
@@ -1,7 +1,12 @@
 
 //https://mapicons.mapsmarker.com icon raussuchen
+//Variblen erstellen
 let myMap = L.map("mapdiv");
-const wienGroup = L.featureGroup().addTo(myMap)
+const url1 = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:CITYBIKEOGD&srsName=EPSG:4326&outputFormat=json";
+const url2 = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json"
+const citybike = L.featureGroup().addTo(myMap)
+const sehenswuerdigkeiten = L.featureGroup().addTo(myMap)
+
 let myLayers = {
     osm : L.tileLayer (
         "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -21,18 +26,6 @@ let myLayers = {
             attribution : "Datenquelle: <a href='https://www.basemap.at'> basemap.at </a>"
         }
     ),
-    bmapgrau : L.tileLayer (
-        "https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
-            subdomains : ["maps","maps1","maps2","maps3","maps4"],
-            attribution : "Datenquelle: <a href='https://www.basemap.at'> basemap.at </a>"
-        }
-    ),
-    bmaphidpi : L.tileLayer (
-        "https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
-            subdomains : ["maps","maps1","maps2","maps3","maps4"],
-            attribution : "Datenquelle: <a href='https://www.basemap.at'> basemap.at </a>"
-        }
-    ),
     bmaporthofoto30cm : L.tileLayer 
     ("https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
         subdomains : ["maps","maps1","maps2","maps3","maps4"],
@@ -48,12 +41,11 @@ myMap.addLayer(myLayers.osm);
 let myMapControl = L.control.layers({ 
     "Openstreetmap" : myLayers.osm,
     "Geolandbasemap" : myLayers.geolandbasemap,
-    "Bmapgrau" : myLayers.bmapgrau,
-    "Bmaphidpi" : myLayers.bmaphidpi,
-    "Bmaporthofoto20cm" : myLayers.bmaporthofoto30cm,
+    "Bmaporthofoto30cm" : myLayers.bmaporthofoto30cm,
 }, {
     "Bmapoverlay" : myLayers.bmapoverlay,
-    "Stationen" : wienGroup, //Hinzufuegen zum Overlay
+    "Citybike Stationen" : citybike, //Hinzufuegen zum Overlay
+    "Sehenwuerdigkeiten" : sehenswuerdigkeiten
 }, {
     collapsed : true
 });
@@ -71,28 +63,52 @@ L.control.scale({
 const myIcon = L.icon({
     iconUrl: "/icons/robbery.png"
 });
+const myIcon2 = L.icon({
+    iconUrl: "/icons/sehenswuerdigkeit.png"
+});
 
-async function addGeojson(url) {
-    const response = await fetch(url);
-    const wienData = await response.json();
-    const geojson = L.geoJSON(wienData, {
+//Citybike
+async function addGeojson(url1) {
+    const response = await fetch(url1);
+    const citybikeData = await response.json();
+    const geojson = L.geoJSON(citybikeData, {
         style: function(feature){
             return{color: "#ff0000"};
         }, 
         pointToLayer: function(geoJsonPoint, latlng) {
             return L.marker(latlng, {
                 icon: L.icon({
-                    iconUrl: "icons/robbery.png"
+                    iconUrl: "icons/cycling.png"
                 })
             });
         }
     });
-    wienGroup.addLayer(geojson);
-    myMap.fitBounds(wienGroup.getBounds());
-}
+    citybike.addLayer(geojson);
+    myMap.fitBounds(citybike.getBounds());
+};
+addGeojson(url1);
 
 
-const url = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&srsName=EPSG:4326&outputFormat=json&typeName=ogdwien:SPAZIERPUNKTOGD,ogdwien:SPAZIERLINIEOGD"
 
-addGeojson(url);
+//Sehenswuerdigkeiten
+async function addGeojson(url2) {
+    const response2 = await fetch(url2);
+    const sehenswuerdigkeitenData = await response2.json();
+    const geojson2 = L.geoJSON(sehenswuerdigkeitenData, {
+        style: function(feature){
+            return{color: "#ff0000"};
+        }, 
+        pointToLayer: function(geoJsonPoint, latlng) {
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: "icons/sehenswuerdigkeit.png"
+                })
+            });
+        }
+    });
+    sehenswuerdigkeiten.addLayer(geojson2);
+    myMap.fitBounds(sehenswuerdigkeiten.getBounds());
+};
+addGeojson(url2);
+
 
