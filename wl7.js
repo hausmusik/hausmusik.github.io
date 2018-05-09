cd08baf3a0493dcb6c0c4929257c4ec6c441190c
Julian Zwerenz <julian.zw@googlemail.com>
2018-05-09 16:30:16 +0200

    Addons

--- wl7.js
+++ wl7.js
@@ -1,11 +1,16 @@
 
 //https://mapicons.mapsmarker.com icon raussuchen
+//plugin: leaflet-hash, leaflet markercluster
 //Variblen erstellen
 let myMap = L.map("mapdiv");
 const url1 = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:CITYBIKEOGD&srsName=EPSG:4326&outputFormat=json";
 const url2 = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json"
 const citybike = L.featureGroup().addTo(myMap)
 const sehenswuerdigkeiten = L.featureGroup().addTo(myMap)
+var clusterBike = L.markerClusterGroup();
+myMap.addLayer(clusterBike);
+var clusterSehenswuerdigkeiten = L.markerClusterGroup();
+myMap.addLayer(clusterSehenswuerdigkeiten);
 
 let myLayers = {
     osm : L.tileLayer (
@@ -44,8 +49,10 @@ let myMapControl = L.control.layers({
     "Bmaporthofoto30cm" : myLayers.bmaporthofoto30cm,
 }, {
     "Bmapoverlay" : myLayers.bmapoverlay,
-    "Citybike Stationen" : citybike, //Hinzufuegen zum Overlay
-    "Sehenwuerdigkeiten" : sehenswuerdigkeiten
+    //"Citybike Stationen" : citybike, //Hinzufuegen zum Overlay
+    //"Sehenwuerdigkeiten" : sehenswuerdigkeiten,
+    "Citybike Cluster" : clusterBike,
+    "Sehenswuerdigkeiten Cluster" : clusterSehenswuerdigkeiten,
 }, {
     collapsed : true
 });
@@ -83,8 +90,11 @@ async function addGeojson(url1) {
             });
         }
     });
-    citybike.addLayer(geojson);
-    myMap.fitBounds(citybike.getBounds());
+    clusterBike.addLayer(geojson);
+    //myMap.fitBounds(citybike.getBounds());
+    var hash = new L.Hash(myMap);
+    
+
 };
 addGeojson(url1);
 
@@ -106,8 +116,8 @@ async function addGeojson2(url2) {
             });
         }
     });
-    sehenswuerdigkeiten.addLayer(geojson2);
-    myMap.fitBounds(sehenswuerdigkeiten.getBounds());
+    clusterSehenswuerdigkeiten.addLayer(geojson2);
+    //myMap.fitBounds(sehenswuerdigkeiten.getBounds());
 };
 addGeojson2(url2);
 
