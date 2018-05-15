ac60509454106f623a37b780fcd24578497ca465
Julian Zwerenz <julian.zw@googlemail.com>
2018-05-15 15:28:29 +0200

    Unbefriedigender Zwischenstand

--- 32/biketirol.js
+++ 32/biketirol.js
@@ -1,15 +1,108 @@
-/*
-    Vorbereitung: GPX Track herunterladen und nach GeoJSON konvertieren
-    -------------------------------------------------------------------
-    Datenquelle https://www.data.gv.at/suche/?search-term=bike+trail+tirol&searchIn=catalog
-    Download Einzeletappen / Zur Ressource ...
-    Alle Dateien im unterverzeichnis data/ ablegen
-    Die .gpx Datei der eigenen Etappe als etappe00.gpx speichern
-    Die .gpx Datei über https://mapbox.github.io/togeojson/ in .geojson umwandeln und als etappe00.geojson speichern
-    Die etappe00.geojson Datei in ein Javascript Objekt umwandeln und als etappe00.geojson.js speichern
 
-    -> statt 00 natürlich die eigene Etappe (z.B. 01,02, ...25)
-*/
+//https://mapicons.mapsmarker.com icon raussuchen
+let myMap = L.map("map");
+let bikeGroup = L.featureGroup().addTo(myMap)
+let myLayers = {
+    osm : L.tileLayer (
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            subdomains : ["a","b","c"], 
+            attribution : "Datenquelle: <a href='https://www.openstreetmap.org'> openstreetmap.at </a>"
+        }
+    ),
+    geolandbasemap : L.tileLayer (
+        "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains : ["maps","maps1","maps2","maps3","maps4"],
+            attribution : "Datenquelle: <a href='https://www.basemap.at'> basemap.at </a>"
+        }
+    ),
+    bmapoverlay :  L.tileLayer (
+        "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains : ["maps","maps1","maps2","maps3","maps4"],
+            attribution : "Datenquelle: <a href='https://www.basemap.at'> basemap.at </a>"
+        }
+    ),
+    bmapgrau : L.tileLayer (
+        "https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains : ["maps","maps1","maps2","maps3","maps4"],
+            attribution : "Datenquelle: <a href='https://www.basemap.at'> basemap.at </a>"
+        }
+    ),
+    bmaphidpi : L.tileLayer (
+        "https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
+            subdomains : ["maps","maps1","maps2","maps3","maps4"],
+            attribution : "Datenquelle: <a href='https://www.basemap.at'> basemap.at </a>"
+        }
+    ),
+    bmaporthofoto30cm : L.tileLayer 
+    ("https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
+        subdomains : ["maps","maps1","maps2","maps3","maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'> basemap.at </a>"
+        }
+    ),
+};
+
+
+
+
+myMap.addLayer(myLayers.geolandbasemap);
+let myMapControl = L.control.layers({ 
+    "Openstreetmap" : myLayers.osm,
+    "Geolandbasemap" : myLayers.geolandbasemap,
+    "Bmapgrau" : myLayers.bmapgrau,
+    "Bmaphidpi" : myLayers.bmaphidpi,
+    "Bmaporthofoto20cm" : myLayers.bmaporthofoto30cm,
+}, {
+    "Bmapoverlay" : myLayers.bmapoverlay,
+    "Stationen" : bikeGroup, //Hinzufuegen zum Overlay
+}, {
+    collapsed : false
+});
+
+myMap.addControl(myMapControl);
+
+L.control.scale({
+    position: "bottomleft",
+    maxWidth: 200,
+    metric: true,
+    imperial: false,
+    }
+).addTo(myMap);
+
+const imst = [47.224,10.749];
+const ehrwald = [47.398, 10.920]
+
+const iconStart = L.icon({
+    iconUrl: "data/startfinish.png"
+});
+const iconFinish = L.icon({
+    iconUrl: "data/finish.png"
+});
+
+const markerOptions1 = {
+    title: "Imst - Ehrwald", // feste Optionen für alle
+    opacity: 1,
+    draggable: false,
+    icon: iconStart,
+};
+const markerOptions2 = {
+    title: "Imst - Ehrwald", // feste Optionen für alle
+    opacity: 1,
+    draggable: false,
+    icon: iconFinish,
+};
+L.marker(imst, markerOptions1).addTo(bikeGroup).bindPopup("<p><a href='https://de.wikipedia.org/wiki/Imst'><strong>Imst</strong></a></p><img style = 'width:200px' src='https://www.stern-imst.at/fileadmin/template/images/hotel-imst-content/Home-Hotel-Imst/Winterwandern-Tirol-Imst-Blick-02.jpg' alt='Patscherkofel' />");
+L.marker(ehrwald, markerOptions2).addTo(bikeGroup).bindPopup("<p><a href='https://de.wikipedia.org/wiki/Ehrwald'><strong>Ehrwald</strong></a></p><img style = 'width:200px' src='https://www.zugspitzarena.com/website/var/tmp/image-thumbnails/120000/120661/thumb__lightbox-image/ehrwald-kirchplatz-zugspitze.jpeg' alt='Patscherkofel' />");
+
+myMap.fitBounds(bikeGroup.getBounds());
+
+
+
+let geojson = L.geoJSON(toGeoJSON).addTo(bikeGroup);
+/*geojson.bindPopup(function(layer) {
+    const props = layer.feature.properties;
+    const popupText = `<h1>${props.name}</h1>
+    <p>Temperatur: ${props.LT} °C </p>`;
+    return popupText;*/
 
 // eine neue Leaflet Karte definieren
 
@@ -25,4 +118,4 @@
 
 // Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
 
-// Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen
+// Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen*/
